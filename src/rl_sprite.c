5d0712d44f25013b979ab36bbdac47c2202faa7e
leiradel <andre@leiradella.com>
2019-07-22 22:17:51 +0100

    Named an unnamed structure

--- src/rl_sprite.c
+++ src/rl_sprite.c
@@ -11,7 +11,8 @@ union item_t
   {
     rl_sprite_t sprite;
     uint16_t*   bg;
-  };
+  }
+  data;
   
   item_t* next;
 };
@@ -49,7 +50,7 @@ rl_sprite_t* rl_sprite_create( void )
 {
   if ( num_sprites < RL_MAX_SPRITES )
   {
-    rl_sprite_t* sprite = &free_list->sprite;
+    rl_sprite_t* sprite = &free_list->data.sprite;
     sprites[ num_sprites++ ].item = free_list;
     free_list = free_list->next;
     
@@ -68,11 +69,11 @@ static int compare( const void* e1, const void* e2 )
   const spt_t* s1 = (const spt_t*)e1;
   const spt_t* s2 = (const spt_t*)e2;
   
-  int32_t c1 = s1->item->sprite.flags;
-  int32_t c2 = s2->item->sprite.flags;
+  int32_t c1 = s1->item->data.sprite.flags;
+  int32_t c2 = s2->item->data.sprite.flags;
   
-  c1 = c1 << 16 | s1->item->sprite.layer;
-  c2 = c2 << 16 | s2->item->sprite.layer;
+  c1 = c1 << 16 | s1->item->data.sprite.layer;
+  c2 = c2 << 16 | s2->item->data.sprite.layer;
   
   return c1 - c2;
 }
@@ -86,8 +87,8 @@ void rl_sprites_blit_nobg( void )
   {
     do
     {
-      sptptr->item->sprite.flags &= ~RL_SPRITE_TEMP_INV;
-      sptptr->item->sprite.flags |= sptptr->item->sprite.image == NULL;
+      sptptr->item->data.sprite.flags &= ~RL_SPRITE_TEMP_INV;
+      sptptr->item->data.sprite.flags |= sptptr->item->data.sprite.image == NULL;
       sptptr++;
     }
     while ( sptptr < endptr );
@@ -96,34 +97,34 @@ void rl_sprites_blit_nobg( void )
   qsort( (void*)sprites, num_sprites, sizeof( spt_t ), compare );
   
   item_t guard = { { { 0 } } }; /* Ugh */
-  guard.sprite.flags = RL_SPRITE_UNUSED;
+  guard.data.sprite.flags = RL_SPRITE_UNUSED;
   sprites[ num_sprites ].item = &guard;
   
   sptptr = sprites;
   
   /* Iterate over active and visible sprites and blit them */
   /* flags & 0x0007U == 0 */
-  if ( sptptr->item->sprite.flags == 0 )
+  if ( sptptr->item->data.sprite.flags == 0 )
   {
     do
     {
-      rl_image_blit_nobg( sptptr->item->sprite.image, sptptr->item->sprite.x, sptptr->item->sprite.y );
+      rl_image_blit_nobg( sptptr->item->data.sprite.image, sptptr->item->data.sprite.x, sptptr->item->data.sprite.y );
       sptptr++;
     }
-    while ( sptptr->item->sprite.flags == 0 );
+    while ( sptptr->item->data.sprite.flags == 0 );
   }
   
   num_visible = sptptr - sprites;
   
   /* Jump over active but invisible sprites */
   /* flags & 0x0004U == 0x0000U */
-  if ( ( sptptr->item->sprite.flags & RL_SPRITE_UNUSED ) == 0 )
+  if ( ( sptptr->item->data.sprite.flags & RL_SPRITE_UNUSED ) == 0 )
   {
     do
     {
       sptptr++;
     }
-    while ( ( sptptr->item->sprite.flags & RL_SPRITE_UNUSED ) == 0 );
+    while ( ( sptptr->item->data.sprite.flags & RL_SPRITE_UNUSED ) == 0 );
   }
   
   int new_num_sprites = sptptr - sprites;
@@ -156,8 +157,8 @@ int rl_sprites_blit( void )
   {
     do
     {
-      sptptr->item->sprite.flags &= ~RL_SPRITE_TEMP_INV;
-      sptptr->item->sprite.flags |= sptptr->item->sprite.image == NULL;
+      sptptr->item->data.sprite.flags &= ~RL_SPRITE_TEMP_INV;
+      sptptr->item->data.sprite.flags |= sptptr->item->data.sprite.image == NULL;
       sptptr++;
     }
     while ( sptptr < endptr );
@@ -166,7 +167,7 @@ int rl_sprites_blit( void )
   qsort( (void*)sprites, num_sprites, sizeof( spt_t ), compare );
   
   item_t guard = { { { 0 } } }; /* Ugh */
-  guard.sprite.flags = RL_SPRITE_UNUSED;
+  guard.data.sprite.flags = RL_SPRITE_UNUSED;
   sprites[ num_sprites ].item = &guard;
   
   sptptr = sprites;
@@ -176,28 +177,28 @@ int rl_sprites_blit( void )
   
   /* Iterate over active and visible sprites and blit them */
   /* flags & 0x0007U == 0 */
-  if ( sptptr->item->sprite.flags == 0 )
+  if ( sptptr->item->data.sprite.flags == 0 )
   {
     do
     {
       /* Don't blit any other sprites if the background space is exhausted */
-      if ( sptptr->item->sprite.image->used + total_used > RL_BG_SAVE_SIZE )
+      if ( sptptr->item->data.sprite.image->used + total_used > RL_BG_SAVE_SIZE )
       {
         break;
       }
 
-      total_used += sptptr->item->sprite.image->used;
-      sptptr->item->bg = saved_ptr;
-      saved_ptr = rl_image_blit( sptptr->item->sprite.image, sptptr->item->sprite.x, sptptr->item->sprite.y, saved_ptr );
+      total_used += sptptr->item->data.sprite.image->used;
+      sptptr->item->data.bg = saved_ptr;
+      saved_ptr = rl_image_blit( sptptr->item->data.sprite.image, sptptr->item->data.sprite.x, sptptr->item->data.sprite.y, saved_ptr );
       sptptr++;
     }
-    while ( sptptr->item->sprite.flags == 0 );
+    while ( sptptr->item->data.sprite.flags == 0 );
   }
   
   num_visible = sptptr - sprites;
 
   /* Jump over active and visible sprites that weren't blit */
-  if ( sptptr->item->sprite.flags == 0 )
+  if ( sptptr->item->data.sprite.flags == 0 )
   {
     result = -1;
 
@@ -205,18 +206,18 @@ int rl_sprites_blit( void )
     {
       sptptr++;
     }
-    while ( sptptr->item->sprite.flags == 0 );
+    while ( sptptr->item->data.sprite.flags == 0 );
   }
 
   /* Jump over active but invisible sprites */
   /* flags & 0x0004U == 0x0000U */
-  if ( ( sptptr->item->sprite.flags & RL_SPRITE_UNUSED ) == 0 )
+  if ( ( sptptr->item->data.sprite.flags & RL_SPRITE_UNUSED ) == 0 )
   {
     do
     {
       sptptr++;
     }
-    while ( ( sptptr->item->sprite.flags & RL_SPRITE_UNUSED ) == 0 );
+    while ( ( sptptr->item->data.sprite.flags & RL_SPRITE_UNUSED ) == 0 );
   }
   
   int new_num_sprites = sptptr - sprites;
@@ -249,7 +250,7 @@ void rl_sprites_unblit( void )
   {
     do
     {
-      rl_image_unblit( sptptr->item->sprite.image, sptptr->item->sprite.x, sptptr->item->sprite.y, sptptr->item->bg );
+      rl_image_unblit( sptptr->item->data.sprite.image, sptptr->item->data.sprite.x, sptptr->item->data.sprite.y, sptptr->item->data.bg );
       sptptr--;
     }
     while ( sptptr >= sprites );
